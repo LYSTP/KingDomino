e90a3de47c3186684cb8242b637efb8410ab1d30
omalvasso <malvassooctavio@gmail.com>
2021-05-21 22:49:47 -0300

    Dominio test

--- src/test/DominoTest.java
+++ src/test/DominoTest.java
@@ -5,12 +5,67 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import clases.Domino;
+import clases.Elemento;
 
 public class DominoTest {
 
 	@Test
+	public void testCrearDomino() {
+		Elemento ei = new Elemento("Agua", 0);
+		Elemento ed = new Elemento("Bosque", 0);
+		Domino d = new Domino(ei, ed);
+
+		assertEquals("Agua", d.getElemIzquierdo().getDescripcion());
+		assertEquals("Bosque", d.getElemDerecho().getDescripcion());
+	}
+
+	@Test
 	public void testCrearDominoConCastillo() {
-		Domino d = new Domino(null);
+		Elemento e = new Elemento("Castillo", 0);
+		Domino d = new Domino(e);
+		assertEquals("Castillo", d.getElemIzquierdo().getDescripcion());
+		assertEquals("Castillo", d.getElemDerecho().getDescripcion());
+	}
+
+	@Test
+	public void testCrearDominoConCorona() {
+		Elemento ei = new Elemento("Agua", 1);
+		Elemento ed = new Elemento("Bosque", 0);
+		Domino d = new Domino(ei, ed);
+		assertEquals(1, d.getElemIzquierdo().getCoronas());
+		assertEquals(0, d.getElemDerecho().getCoronas());
+	}
+
+	@Test
+	public void testSePuedeUnirDominos() {
+		Elemento ei1 = new Elemento("Agua", 0);
+		Elemento ed1 = new Elemento("Bosque", 0);
+		Elemento ei2 = new Elemento("Agua", 0);
+		Elemento ed2 = new Elemento("Pastizal", 0);
+		Domino d1 = new Domino(ei1, ed1);
+		Domino d2 = new Domino(ei2, ed2);
+		assertEquals(true, d1.puedenUnir(d2));
+	}
+	
+	@Test
+	public void testNoSePuedeUnirDominos() {
+		Elemento ei1 = new Elemento("Agua", 0);
+		Elemento ed1 = new Elemento("Bosque", 0);
+		Elemento ei2 = new Elemento("Pastizal", 0);
+		Elemento ed2 = new Elemento("Pastizal", 0);
+		Domino d1 = new Domino(ei1, ed1);
+		Domino d2 = new Domino(ei2, ed2);
+		assertEquals(false, d1.puedenUnir(d2));
+	}
+	
+	@Test
+	public void testPuedeUnirDominosPorCastillo() {
+		Elemento ei1 = new Elemento("Agua", 0);
+		Elemento ed1 = new Elemento("Bosque", 0);
+		Elemento ei2 = new Elemento("Castillo", 0);
+		Domino d1 = new Domino(ei1, ed1);
+		Domino d2 = new Domino(ei2);
+		assertEquals(true, d1.puedenUnir(d2));
 	}
 
 }
