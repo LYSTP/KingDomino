43f4b2d64794783fe8c00aa0bb5778ee477c68fc
omalvasso <malvassooctavio@gmail.com>
2021-05-22 01:33:50 -0300

    comentadas

--- src/clases/Sala.java
+++ src/clases/Sala.java
@@ -1,43 +1,43 @@
-package clases;
-import java.util.List;
-
-public class Sala {
-	private int id;
-	private String descripcion;
-	private List<Jugador> jugadores;
-	private int cantPartipantes;
-
-	public Sala(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-	public Sala(String descripcion, Jugador jugador) {
-		this.descripcion = descripcion;
-		this.jugadores.add(jugador);
-	}
-
-	public List<Jugador> getJugadores() {
-		return jugadores;
-	}
-
-	public int getCantPartipantes() {
-		return cantPartipantes;
-	}
-
-	boolean agregarJugador(Jugador j) {
-		if (this.jugadores.size() <= this.cantPartipantes) {
-			this.jugadores.add(j);
-			return true;
-		}
-		return false;
-	}
-
-	boolean quitarJugador(Jugador j) {
-		if (this.jugadores.size() > 1) {
-			this.jugadores.remove(j);
-			return true;
-		}
-		return false;
-	}
-
-}
+//package clases;
+//import java.util.List;
+//
+//public class Sala {
+//	private int id;
+//	private String descripcion;
+//	private List<Jugador> jugadores;
+//	private int cantPartipantes;
+//
+//	public Sala(String descripcion) {
+//		this.descripcion = descripcion;
+//	}
+//
+//	public Sala(String descripcion, Jugador jugador) {
+//		this.descripcion = descripcion;
+//		this.jugadores.add(jugador);
+//	}
+//
+//	public List<Jugador> getJugadores() {
+//		return jugadores;
+//	}
+//
+//	public int getCantPartipantes() {
+//		return cantPartipantes;
+//	}
+//
+//	boolean agregarJugador(Jugador j) {
+//		if (this.jugadores.size() <= this.cantPartipantes) {
+//			this.jugadores.add(j);
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	boolean quitarJugador(Jugador j) {
+//		if (this.jugadores.size() > 1) {
+//			this.jugadores.remove(j);
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//}
