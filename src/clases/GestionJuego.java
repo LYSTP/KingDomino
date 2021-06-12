66881dbdd1900ac2e3baef6565fca5a4b890bc27
Lucas <villegaslucas93@gmail.com>
2021-06-11 22:02:40 -0300

    K2

--- src/clases/GestionJuego.java
+++ src/clases/GestionJuego.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import sun.jvm.hotspot.ObjectHistogram;
-
 public class GestionJuego {
 
 	List<Jugador> jugadores;
